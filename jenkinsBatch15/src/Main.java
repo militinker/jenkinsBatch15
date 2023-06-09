c2eaee55d6006d8cf7415ece4b7992118e5fee4a
militinker <militinker@gmail.com>
2023-06-09 15:24:32 -0400

    initial commit

---
+++ jenkinsBatch15/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+
+
+        for (int i = 1; i < 5; i++) {
+            System.out.println("hello world");
+            Thread.sleep(2000);
+        }
+    }
+}
